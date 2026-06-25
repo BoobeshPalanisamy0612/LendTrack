c85e79e5576b52cb6e31357194414b5bb83b1fca
Boobesh Palanisamy <boobeshpalanisamy0612@gmail.com>
2026-06-25 14:58:46 +0530

    Backend setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      autoIndex: true,
-    });
+    console.log('Mongo URI loaded:', !!process.env.MONGO_URI);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
+    console.error('FULL ERROR:');
+    console.error(error);
     process.exit(1);
   }
 };
