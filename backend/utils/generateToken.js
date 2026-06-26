6ce81588d74519714c64e98f4cac1dbcc0144a2d
Boobesh Palanisamy <boobeshpalanisamy0612@gmail.com>
2026-06-26 10:50:23 +0530

    update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,17 +1,4 @@
-// const jwt = require('jsonwebtoken');
-
-// const generateToken = (id) => {
-//   return jwt.sign({ id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRE || '7d',
-//   });
-// };
-
-// module.exports = generateToken;
-
-
 const jwt = require('jsonwebtoken');
-
-console.log("JWT_SECRET =", process.env.JWT_SECRET);
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
